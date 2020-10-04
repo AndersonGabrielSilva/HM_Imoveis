a4e7670d5494d9ed1b209e64fe36fd7f0bc12c06
anderson <andersongabriel_maria@hotmail.com>
2020-10-03 21:45:03 -0300

    Tela de Login

---
+++ app/src/main/java/com/helptechnology/hmimveis/view/activity_tela_login.java
@@ -0,0 +1,17 @@
+package com.helptechnology.hmimveis.view;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.helptechnology.hmimveis.R;
+
+public class activity_tela_login extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+    }
+}
